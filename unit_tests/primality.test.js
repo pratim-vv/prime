09500bdf3e3380230e5c8986878cca4973337601
pratim-vv <pratimvasireddy@gmail.com>
2025-05-23 16:12:36 -0500

    added test cases for  greatestCommonDivisor function

--- unit_tests/primality.test.js
+++ unit_tests/primality.test.js
@@ -1,15 +1,6 @@
-const { modPow, isPrimeFermat } = require('../prime.js');
-
-describe('modPow', () => {
-    test('correctly computes modular exponentiation', () => {
-        expect(modPow(2, 3, 5)).toBe(8 % 5); // 8 % 5 = 3
-        expect(modPow(3, 4, 7)).toBe(81 % 7); // 81 % 7 = 4
-        expect(modPow(5, 0, 13)).toBe(1);
-    });
-});
+const { isPrimeFermat, modPow, greatestCommonDivisor } = require('../prime.js');
 
 describe('isPrimeFermat', () => {
-
     test('returns true for primes', () => {
         expect(isPrimeFermat(2, 3)).toBe(true);
         expect(isPrimeFermat(2, 5)).toBe(true);
@@ -26,5 +17,22 @@ describe('isPrimeFermat', () => {
         // not all bases fool Carmichael numbers, must be a base coprime to carmichael number
         expect(isPrimeFermat(2, 561)).toBe(true); 
         expect(isPrimeFermat(53, 63973)).toBe(true);
+    });
+});
+
+describe('modPow', () => {
+    test('correctly computes modular exponentiation', () => {
+        expect(modPow(2, 3, 5)).toBe(8 % 5); // 8 % 5 = 3
+        expect(modPow(3, 4, 7)).toBe(81 % 7); // 81 % 7 = 4
+        expect(modPow(5, 0, 13)).toBe(1);
+    });
+});
+
+describe('greatestCommonDivisor', () => {
+    test('correctly finds greatest common denominator', () => {
+        expect(greatestCommonDivisor(12, 8)).toBe(4);
+        expect(greatestCommonDivisor(8, 12)).toBe(4);
+        expect(greatestCommonDivisor(838673879, 947304261)).toBe(1);
+        expect(greatestCommonDivisor(2738502576, 374736)).toBe(48);
     });
 });
