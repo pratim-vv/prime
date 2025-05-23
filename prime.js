e1a274471a1755793cf4d3a992cab205a874c785
pratim-vv <pratimvasireddy@gmail.com>
2025-05-23 15:58:04 -0500

    added implementation of miller-rabin primality testing

--- prime.js
+++ prime.js
@@ -6,6 +6,43 @@ function isPrimeFermat(a, n) {
     return true;
 }
 
+// Probilistic Algorithm for Testing Primality paper
+function isPrimeMillerRabin(n, attempts=20) {
+    if (n <= 1) return false;
+    if (n % 2 === 0) return false;
+
+    // small primes hardcoded for efficiency-sake
+    if (n === 2 || n === 3) return true;
+    if (n === 5 || n === 7) return true;
+    if (n === 11 || n === 13) return true;
+    if (n === 17 || n === 19) return true;
+
+    // variables as denoted in original paper
+    let k =  n - 1;
+    let i = 0;
+    while (k % 2 === 0) {
+        k = Math.floor(k / 2);
+        i++;
+    }
+
+    for (let j = 0; j < attempts; j++) {
+
+        // b is primality witness
+        let b = randomRange(1, n - 1);
+
+        // Check fermat primality first
+        if(!isPrimeFermat(b, n)) return false;
+
+        let gcd = greatestCommonDivisor(modPow(b, k, n) - 1, n);
+        if (gcd === 1 || gcd === n) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
 // Helper function for modular exponentiation
 function modPow(base, exponent, mod) {
     let result = 1;
@@ -20,4 +57,23 @@ function modPow(base, exponent, mod) {
     return result;
 }
 
-module.exports = { modPow, isPrimeFermat };
+// Helper function for random integer in [min, max]
+function randomRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Helper function for finding Greatest Common Divisor using Euclidean Method
+function greatestCommonDivisor(a, b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    while (b) {
+        let t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
+
+module.exports = {isPrimeFermat, isPrimeMillerRabin, modPow, randomRange, greatestCommonDivisor };
+
